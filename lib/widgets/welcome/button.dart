ec2f6021f40b7f29889de0e344f3322e11254a8c
3mr M7mud <amrosy00@gmail.com>
2022-07-19 21:30:23 +0200

    Some changes

---
+++ lib/widgets/welcome/button.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/constants/size.dart';
+
+class Button extends StatelessWidget {
+  final String label;
+  final void Function() onPressed;
+  final bool border;
+
+  const Button({
+    Key? key,
+    required this.label,
+    required this.onPressed,
+    this.border = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: border
+          ? OutlinedButton.styleFrom(
+              elevation: 0,
+              fixedSize:
+                  Size(SizeConfig.screenWidth, SizeConfig.screenHeight * 0.075),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  side: const BorderSide(
+                    color: Colors.white,
+                  )),
+            )
+          : ElevatedButton.styleFrom(
+              elevation: 15,
+              fixedSize:
+                  Size(SizeConfig.screenWidth, SizeConfig.screenHeight * 0.075),
+              shadowColor: Theme.of(context).primaryColor.withOpacity(0.5),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ),
+      onPressed: onPressed,
+      child: Text(
+        label,
+        style: Theme.of(context)
+            .textTheme
+            .titleMedium!
+            .copyWith(color: Colors.white),
+      ),
+    );
+  }
+}
